6fb4128019d3ff0db9a4251ce2b97855bf0d36cd
cnguyenm <minhchauvtvn95@gmail.com>
2018-08-29 01:58:52 +0700

    test2

--- audio.sh
+++ audio.sh
@@ -1,56 +1,22 @@
 
-sudo apt-get install sox
-sudo apt-get install python-pyaudio
-sudo apt-get install libsox-fmt-mp3
-
-sudo pip install wikipedia
-sudo pip install pygoogle
-sudo pip install nltk
-sudo pip install sympy
-
-sudo pip2 install pyalsaaudio
-
-sudo pip2 install pygst
-sudo apt-get install python-gst0.10 gstreamer0.10-plugins-good gstreamer0.10-plugins-ugly
-sudo apt-get install python-gst0.10 gstreamer0.10-plugins-good 
+apt-get install -qq python python-dev python-pip build-essential swig libpulse-dev python-dev libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev libportaudio2 libportaudiocpp0 portaudio19-dev python-pyaudio python3-pyaudio portaudio19-dev python-all-dev python3-all-dev sox python-pyaudio libsox-fmt-mp3 python-gst0.10 gstreamer0.10-plugins-good gstreamer0.10-plugins-ugly python-gst0.10 gstreamer0.10-plugins-good python-pyaudio libsox-fmt-mp3
 
 pip install SpeechRecognition
-
-git clone https://github.com/Uberi/speech_recognition
-
-cd speech_recognition/
-
-git clone http://people.csail.mit.edu/hubert/git/pyaudio.git
-sudo apt-get install libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev
-cd pyaudio/
-
-sudo apt-get install  libportaudio2 libportaudiocpp0 portaudio19-dev
-
-sudo apt-get install python-dev
-sudo python pyaudio/setup.py install
-sudo python setup.py install
-cd ..
-cd examples/
-
-sudo apt-get remove bluez-alsa
-sudo python microphone_recognition.py 
-
-#nano test1.py
-
-pip install pyttsx
-sudo python test1.py 
-pip3 install pyttsx
-pip2 install pyttsx
-
+pip install wikipedia
+pip install pygoogle
+pip install nltk
+pip install sympy
+pip2 install pyalsaaudio
+pip2 install pygst
 pip install pyttsx3
-
-python3 check.py 
+pip install pocketsphinx
+#pip install --upgrade pocketsphinx
  
-sudo apt-get remove --auto-remove jack
+apt-get remove bluez-alsa
+apt-get remove --auto-remove jack
 jack_control  stop
-sudo python3 write_audio.py 
- 
-sudo apt-get install -qq python python-dev python-pip build-essential swig libpulse-dev
-sudo pip install pocketsphinx
-pip install --upgrade pocketsphinx
+
+#git clone http://people.csail.mit.edu/hubert/git/pyaudio.git
+#python pyaudio/setup.py install
+#git clone https://github.com/Uberi/speech_recognition
 
